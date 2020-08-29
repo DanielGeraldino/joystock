ee8a42437e5617521f3c5925444e4a5ca185b9f3
Daniel GGuerra <danielguerra@ucl.br>
2020-08-29 16:12:34 -0300

    segundo commit

---
+++ app/src/main/java/dospropleys/android/joystock/MainActivity.kt
@@ -0,0 +1,11 @@
+package dospropleys.android.joystock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
