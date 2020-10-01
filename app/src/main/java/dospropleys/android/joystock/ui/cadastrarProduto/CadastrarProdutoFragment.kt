ab725ded8e25596d16ee00170a6a3d197dd01fad
Daniel GGuerra <danielguerra@ucl.br>
2020-10-01 10:33:54 -0300

    Tela cadastro item

--- app/src/main/java/dospropleys/android/joystock/ui/cadastrarProduto/CadastrarProdutoFragment.kt
+++ app/src/main/java/dospropleys/android/joystock/ui/cadastrarProduto/CadastrarProdutoFragment.kt
@@ -2,13 +2,19 @@ package dospropleys.android.joystock.ui.cadastrarProduto
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.google.android.gms.common.internal.Objects
 import dospropleys.android.joystock.R
+import kotlinx.android.synthetic.main.cadastrar_produto_fragment.view.*
 
-class CadastrarProdutoFragment : Fragment() {
+class CadastrarProdutoFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     companion object {
         fun newInstance() = CadastrarProdutoFragment()
@@ -20,13 +26,35 @@ class CadastrarProdutoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.cadastrar_produto_fragment, container, false)
+        val root = inflater.inflate(R.layout.cadastrar_produto_fragment, container, false)
+
+        ArrayAdapter.createFromResource(
+            root.context,
+            R.array.lista_unidades,
+            android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                root.spinnerUnidades.adapter = adapter }
+
+        root.spinnerUnidades.onItemSelectedListener = this
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CadastrarProdutoViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val teste = parent?.getItemAtPosition(position).toString()
+        Toast.makeText(activity, teste, Toast.LENGTH_LONG).show()
+        Log.d("spinner", teste)
     }
 
 }
